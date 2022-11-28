336bbea2530209d80e5361a4655af7153d86d3ae
Cecile Dai <ceciled126@gmail.com>
2022-11-28 14:08:02 -0500

    added more program buttons

--- src/routes/MajorsAndMinorsPage.tsx
+++ src/routes/MajorsAndMinorsPage.tsx
@@ -10,21 +10,78 @@ export const MajorsAndMinorsPage = () => {
       <div className="title">MAJORS & MINORS</div>
       <BackButton />
       <div className="mt-5">
-        <button className="degree-button" onClick={() => window.open('https://www.mcgill.ca/study/2022-2023/faculties/engineering/undergraduate/ug_eng_electrical_computer_engineering','_blank')}>
+        <button className="degree-button" onClick={() => setMajorsButtonsOpen(!majorsButtonsOpen)}>
           <div className="button-text">
             VIEW ECSE MAJORS
           </div>
         </button>
-        <button className="degree-button" onClick={() => window.open('https://www.mcgill.ca/study/2022-2023/faculties/engineering/undergraduate/ug_eng_minor_programs','_blank')}>
+        {majorsButtonsOpen && (
+          <>
+            <button className="degree-button" style={{ backgroundColor: '#e8dcf1' }} onClick={() => window.open('https://www.mcgill.ca/study/2022-2023/faculties/engineering/undergraduate/ug_eng_electrical_computer_engineering', '_blank')}>
+              <div className="button-text">
+                ELECTRICAL
+              </div>
+            </button>
+            <button className="degree-button" style={{ backgroundColor: '#e8dcf1' }} onClick={() => window.open('https://www.mcgill.ca/study/2022-2023/faculties/engineering/undergraduate/ug_eng_electrical_computer_engineering', '_blank')}>
+              <div className="button-text">
+                COMPUTER
+              </div>
+            </button>
+            <button className="degree-button" style={{ backgroundColor: '#e8dcf1' }} onClick={() => window.open('https://www.mcgill.ca/study/2022-2023/faculties/engineering/undergraduate/ug_eng_electrical_computer_engineering', '_blank')}>
+              <div className="button-text">
+                SOFTWARE
+              </div>
+            </button>
+          </>
+        )}
+        <button className="degree-button" onClick={() => setMinorsButtonsOpen(!minorsButtonsOpen)}>
           <div className="button-text">
             VIEW ECSE MINORS
           </div>
         </button>
-        <button className="degree-button" onClick={() => window.open('https://www.mcgill.ca/study/2022-2023/faculties/engineering/undergraduate/programs/bachelor-engineering-beng-minor-software-engineering','_blank')}>
+        {minorsButtonsOpen && (
+          <>
+            <button className="degree-button" style={{ backgroundColor: '#e8dcf1' }} onClick={() => window.open('https://www.mcgill.ca/study/2022-2023/faculties/engineering/undergraduate/ug_eng_electrical_computer_engineering', '_blank')}>
+              <div className="button-text">
+                ELECTRICAL
+              </div>
+            </button>
+            <button className="degree-button" style={{ backgroundColor: '#e8dcf1' }} onClick={() => window.open('https://www.mcgill.ca/study/2022-2023/faculties/engineering/undergraduate/ug_eng_electrical_computer_engineering', '_blank')}>
+              <div className="button-text">
+                COMPUTER
+              </div>
+            </button>
+            <button className="degree-button" style={{ backgroundColor: '#e8dcf1' }} onClick={() => window.open('https://www.mcgill.ca/study/2022-2023/faculties/engineering/undergraduate/ug_eng_electrical_computer_engineering', '_blank')}>
+              <div className="button-text">
+                SOFTWARE
+              </div>
+            </button>
+          </>
+        )}
+        <button className="degree-button" onClick={() => setOutsideMinorsButtonsOpen(!outsideMinorsButtonsOpen)}>
           <div className="button-text">
             VIEW NON-ECSE MINORS
           </div>
         </button>
+        {outsideMinorsButtonsOpen && (
+          <>
+            <button className="degree-button" style={{ backgroundColor: '#e8dcf1' }} onClick={() => window.open('https://www.mcgill.ca/study/2022-2023/faculties/engineering/undergraduate/ug_eng_electrical_computer_engineering', '_blank')}>
+              <div className="button-text">
+                ARTS/SCI
+              </div>
+            </button>
+            <button className="degree-button" style={{ backgroundColor: '#e8dcf1' }} onClick={() => window.open('https://www.mcgill.ca/study/2022-2023/faculties/engineering/undergraduate/ug_eng_electrical_computer_engineering', '_blank')}>
+              <div className="button-text">
+                EDUCATION
+              </div>
+            </button>
+            <button className="degree-button" style={{ backgroundColor: '#e8dcf1' }} onClick={() => window.open('https://www.mcgill.ca/study/2022-2023/faculties/engineering/undergraduate/ug_eng_electrical_computer_engineering', '_blank')}>
+              <div className="button-text">
+                OTHER
+              </div>
+            </button>
+          </>
+        )}
       </div>
     </div>
   )
